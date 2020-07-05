4fab6e67196e1ff2b64556fa324cd9a8291d5e41
Kolamin <gofranich@gmail.com>
2020-07-06 00:10:02 +0600

    lesson 8

--- src/App.js
+++ src/App.js
@@ -3,27 +3,33 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
-  return (
-    <div>
-      <div><Header /></div>
-      <div className="App">
-        <ul>
-          <li>css</li>
-          <li>html</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+}
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+            </ul>
+        </div>
+    )
+}
 
 const Header = () => {
-  return (
-    <div>
-      <a href="#">Home</a>
-      <a href="#">News Feed</a>
-      <a href="#">Messages</a>
-    </div>
-  );
+    return (
+        <div>
+            <a href="#">Home</a>
+            <a href="#">News Feed</a>
+            <a href="#">Messages</a>
+        </div>
+    );
 };
 
 export default App;
