54c5abb95af9e86cdd8481578092bb56d1fbefc9
Kolamin <gofranich@gmail.com>
2020-07-07 18:35:35 +0600

    Import lesson

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Header from "./Header.js";
 import Technologies from "./Technologies.js";
+import Header from "./Header";
 
 const App = () => {
     return (
         <div>
-            <Header/>
+           <Header />
             <Technologies/>
         </div>
     );
