cd88565683fb98710f38aceb12ec8c6916f8a291
Kolamin <gofranich@gmail.com>
2020-07-07 20:32:44 +0600

    Verstka grid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import Technologies from "./Technologies.js";
-import Header from "./Header";
 
 const App = () => {
-    return (
+  return (
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://static.rfstat.com/renderforest/images/v2/logo-homepage/flat_3.png" />
+      </header>
+      <nav className="nav">
         <div>
-           <Header />
-            <Technologies/>
+          <a>Profile</a>
         </div>
-    );
-}
+        <div>Messages</div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" />
+      </div>
+    </div>
+  );
+};
 export default App;
