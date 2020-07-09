fd28b8b3688058abf8c8234f08a54a7fc07cee88
Kolamin <gofranich@gmail.com>
2020-07-09 08:45:14 +0600

    Make omponent

--- src/App.js
+++ src/App.js
@@ -1,50 +1,16 @@
 import React from "react";
 import "./App.css";
+import Header from "./component/Header";
+import Navbar from "./component/Navbar";
+import Profile from "./component/Profile";
 
 const App = () => {
-  return (
-    <div className="app-wrapper">
-      <header className="header">
-        <img src="https://static.rfstat.com/renderforest/images/v2/logo-homepage/flat_3.png" />
-      </header>
-      <nav className="nav">
-        <div>
-          <a>Profile</a>
+    return (
+        <div className="app-wrapper">
+            <Header />
+            <Navbar />
+            <Profile />
         </div>
-        <div>Messages</div>
-        <div>
-          <a>News</a>
-        </div>
-        <div>
-          <a>Music</a>
-        </div>
-        <div>
-          <a>Settings</a>
-        </div>
-      </nav>
-      <div className="content">
-        <div>
-          <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" />
-        </div>
-        <div>
-            ava + decsription
-        </div>
-      </div>
-      <div>
-        My posts
-        <div>
-        New posts
-        </div>
-      </div>
-      <div>
-        <div>
-            post 1
-        </div>
-        <div>
-            post 2
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 export default App;
---
+++ src/component/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className="content">
+        <div>
+            <img
+                src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New posts
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
